ff93e4602d839048971f91dd5a66ad8b4da090ff
Babykik <omotoyosiolufunmi@gmail.com>
2022-09-11 08:04:54 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main -Entry point
- *
- *  Return: Always 0 (successful)
+ * main - prints out a string to stdout
+ *  Return: 0 if success
  */
 int main(void)
 {
-	puts("\"programming is like b
-
-			return (0)
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
